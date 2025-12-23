dd5e323d1aed6a05e6c4552903a2744bf47d5ca7
LReyes1593 <luis.r.p@uinenlinea.mx>
2025-12-23 15:32:57 -0600

    Update HolaMundo.py

--- HolaMundo.py
+++ HolaMundo.py
@@ -3,9 +3,12 @@ import pandas as pd
 from PIL import Image
 import altair as alt
 
-
+logo = Image.open("LogoPANAM.jpg")
 
 st.title("Hola Streamlit")
+
+with col2:
+    st.image(logo, width=160)
 
 
 
